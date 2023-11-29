2da26991f47585589ca1f5d3f144584ec2621960
lallue <alluel@3il.fr>
2023-11-29 09:46:29 +0100

    exo1

--- exo1/Main.java
+++ exo1/Main.java
@@ -1,5 +1,8 @@
 package exo1;
 
 public class Main {
-
+	public static void main (String [] args ) {
+		PointA pt = new PointA( 3,2);
+		pt.afficher();
+	}
 }
--- exo1/PointA.java
+++ exo1/PointA.java
@@ -1,5 +1,10 @@
 package exo1;
 
-public class PointA {
-
+public class PointA extends Point {
+	public PointA (int x, int y ) {
+		super.initialise(x,y);
+	}
+	public void afficher () {
+	System.out.println( "x : " + getX() + " y : " + getY() );
+}
 }
